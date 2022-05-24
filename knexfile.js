d496a82fb04bfcae00f52e35185494ce9fb2da66
luizpbello <luizpaulopinto25@gmail.com>
2022-05-23 21:22:07 -0300

    'knex'

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
 // Update with your config settings.
-const { db } = require('./.env')
+//
 
 module.exports = {  
     client: 'postgresql',
-    connection: db,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
